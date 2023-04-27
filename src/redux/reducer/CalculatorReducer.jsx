4984481ead1317718967c00d528686b9134a1ff3
Aidana777 <aidananiazalieva27@gmail.com>
2023-04-27 22:22:04 +0600

    Done!!

--- src/redux/reducer/CalculatorReducer.jsx
+++ src/redux/reducer/CalculatorReducer.jsx
@@ -7,29 +7,46 @@ export const CalculatorReducer = (state = initialState, action) => {
 
   switch (action.type) {
     case types.PLUS:
+      if (!action.payload.numOne || !action.payload.numTwo) {
+        return { answer: "Введите число!" };
+      }
       return {
         ...state,
-        answer: +action.payload.numOne + +action.payload.numTwo
+        answer: +action.payload.numOne + +action.payload.numTwo,
       }
 
+
     case types.MINUS:
+
+      if (!action.payload.numOne || !action.payload.numTwo) {
+        return { answer: "Введите число!" };
+      }
       return {
         ...state,
         answer: +action.payload.numOne - +action.payload.numTwo,
       }
 
     case types.MULTIPLIED:
+
+
+
+      if (!action.payload.numOne || !action.payload.numTwo) {
+        return { answer: "Введите число!" };
+      }
       return {
         ...state,
-        answer: +action.payload.numOne * +action.payload.numTwo
+        answer: +action.payload.numOne * +action.payload.numTwo,
       }
     case types.DIVIDED:
+      if (!action.payload.numOne || !action.payload.numTwo) {
+        return { answer: "Введите число!" };
+      }
       return {
         ...state,
         answer: (+action.payload.numOne / +action.payload.numTwo).toFixed(1),
       }
 
-   
+
     default: return state
   }
 
